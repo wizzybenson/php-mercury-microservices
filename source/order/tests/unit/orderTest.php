55c7089c77fff7bba5061ff6de1cf4cd849a6410
arouay <arouayabdelalim@gmail.com>
2020-06-24 05:49:26 +0100

    add unit test files for order's/refund's methods

---
+++ source/order/tests/unit/orderTest.php
@@ -0,0 +1,40 @@
+<?php 
+class orderTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+    public function testGetCart(){
+
+    }
+    public function testGetOrdersBy(){
+
+    }
+    public function testMakePayment(){
+
+    }
+    public function testChangeStatus(){
+
+    }
+    public function testSave(){
+
+    }
+    public function testDelete(){
+
+    }
+}
