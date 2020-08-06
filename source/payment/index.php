6fd89e59562d6a7b71c21fe4b8aebc0919ff0b57
Lagrida <lagyassine@gmail.com>
2020-08-06 17:52:03 +0100

    adding front end

--- source/payment/index.php
+++ source/payment/index.php
@@ -5,10 +5,4 @@ $config=include_once ROOT.'config/config.php';
 require_once ROOT.'./../vendor/autoload.php';
 require_once ROOT.'config/services.php';
 \Ubiquity\controllers\Startup::run($config);
-try{
-   $database = new PDO('mysql:host=microservice_payment_database','root','mysecret', [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'']);
-}catch(PDOexception $e){
-   exit('<h1 style="text-align:center; color: #CC0000; margin-top: 15px">DataBase Error</h1>');
-}
-$database->exec("CREATE DATABASE paymentdb IF NOT EXISTS");
-?>
+
